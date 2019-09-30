0fc0ca18784a308e3a48eed164a0c3daeba13ee5
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-09-30 11:51:57 +0200

    initial

---
+++ RazorPagesGrades/Services/IGradebook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RazorPagesGrades.Models;
+
+namespace RazorPagesGrades.Services
+{
+    public interface IGradebook
+    {
+        IEnumerable<Grade> GetAllGrades();
+        IEnumerable<Grade> GetGrades(string subjectAcronym);
+        bool AddGrade(string acronym, double value, int weight);
+        bool AddGrade(Grade grade);
+        bool RemoveGrade(Guid id);
+    }
+}
